ffbf597a62d7aef5e5c3a8ec6137f555c74f6c72
paruljamwal <paruljamwal2000@gmail.com>
2022-10-17 00:18:43 +0530

    changes

--- src/Model/song.model.js
+++ src/Model/song.model.js
@@ -8,7 +8,7 @@ const SongSchema = new mongoose.Schema(
       cover: { type: String, required: true },
       userId:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
       artistId:[{type:mongoose.Schema.Types.ObjectId,ref:"Artist"}],
-      rating:{type:mongoose.Schema.Types.ObjectId,ref:"Rating"}
+      rating:{type:Number}
     },
     {
       timestamps: true,
